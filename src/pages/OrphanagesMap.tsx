93ba2c9f36d7753a4cafa06ffacda800434362cf
Marlon Paulo <marlonpaulo1@gmail.com>
2020-10-13 11:20:08 -0300

    Criação da landing page e da page de map

---
+++ src/pages/OrphanagesMap.tsx
@@ -0,0 +1,44 @@
+import React from 'react';
+import { Link } from 'react-router-dom' 
+import { FiPlus } from 'react-icons/fi'
+import { Map, TileLayer } from 'react-leaflet'
+
+import 'leaflet/dist/leaflet.css'
+
+import mapMarkerImg from '../img/map-marker.svg'
+
+import '../style/pages/orphanages-map.css'
+
+function OrphanagesMap() {
+    return (
+        <div id="page-map">
+            <aside>
+                <header>
+                    <img src={mapMarkerImg} alt="Happy"/>
+
+                    <h2>Escolha um orfanato no mapa</h2>
+                    <p>Muitas crianças estão esperando a sua visita</p>
+                </header>
+
+                <footer>
+                    <strong>Cabo de Santo Agostinho</strong>
+                    <span>Santa Catarina</span>
+                </footer>
+            </aside>
+
+            <Map 
+                center={[-8.2921647,-35.0278015]}
+                zoom={15}
+                style={{ width: '100%', height: '100%' }}
+            >
+                <TileLayer url="https://a.tile.openstreetmap.org/{z}/{x}/{y}.png" />
+            </Map>
+
+            <Link to="" className="create-orphanage">
+                <FiPlus size={32} color="#fff" />
+            </Link>
+        </div>
+    )
+}
+
+export default OrphanagesMap;
